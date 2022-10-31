7def5923496e20cbb250b9fb2c369534a05d33c5
derek-le1105 <derekle1105@gmail.com>
2022-10-31 08:57:19 -0700

    Finished color visualization for merge sort

--- src/Visualizer/HelperFunctions/checkArray.js
+++ src/Visualizer/HelperFunctions/checkArray.js
@@ -4,19 +4,23 @@ const prevColor = '#C9B79C';
 const correctColor = `#899886`;
 
 const checkArray = async (array) => {
+  let comparisons = 0,
+    accesses = 0;
   for (let i = 0; i < array.length; i++) {
     let currBar = document.getElementById(`bar-${i}`);
     currBar.style.backgroundColor = correctColor;
     await asyncTimeout({ timeout: 10 });
+    ++comparisons;
+    accesses += 2;
     if (array[i] > array[i + 1]) {
       for (let j = i; j >= 0; --j) {
         currBar = document.getElementById(`bar-${j}`);
         currBar.style.backgroundColor = prevColor;
       }
-      return false;
+      return [false, comparisons, accesses];
     }
   }
-  return true;
+  return [true, comparisons, accesses];
 };
 
 export default checkArray;
--- src/Visualizer/SortingAlgorithms/mergeSort.js
+++ src/Visualizer/SortingAlgorithms/mergeSort.js
@@ -1,9 +1,9 @@
 import asyncTimeout from '../HelperFunctions/asyncTimeout';
+import checkArray from '../HelperFunctions/checkArray';
 
 const prevColor = '#C9B79C';
 const compareColor = '#574638';
-const wrongColor = '#B42D43';
-const correctColor = `#899886`;
+const iterColor = '#B42D43';
 
 let arrayCopy = [];
 //TODO: Set up color coding
@@ -13,30 +13,23 @@ const mergeSort = async ({
   setRandomArray,
   sortSpeed,
   setArrayComparisons,
+  setArrayAccesses,
 }) => {
   let leftIdx = 0,
     rightIdx = randomArray.length;
   arrayCopy = randomArray.slice();
   await splitArray(leftIdx, rightIdx, setRandomArray, sortSpeed);
+  await checkArray(arrayCopy);
 };
 
 const splitArray = async (leftIdx, rightIdx, setRandomArray, sortSpeed) => {
   if (rightIdx - leftIdx <= 1) return;
 
   let middle = Math.floor((leftIdx + rightIdx) / 2);
-  console.log(leftIdx, middle);
   await splitArray(leftIdx, middle, setRandomArray, sortSpeed);
-  console.log(middle, rightIdx);
   await splitArray(middle, rightIdx, setRandomArray, sortSpeed);
 
   await mergeArray(leftIdx, middle, rightIdx, setRandomArray, sortSpeed);
-};
-
-const changeColor = (...args) => {
-  let color = args[args.length - 1];
-  for (let i = 0; i < args.length - 1; i++) {
-    args[i].style.backgroundColor = color;
-  }
 };
 
 const mergeArray = async (
@@ -51,52 +44,62 @@ const mergeArray = async (
   let leftArr = arrayCopy.slice(leftIdx, middle);
   let rightArr = arrayCopy.slice(middle, rightIdx);
   let tempArray = [];
-  let bar1, bar2;
+  let leftBar, rightBar, bar1, bar2;
+
+  leftBar = document.getElementById(`bar-${leftIdx}`);
+  leftBar.style.background = compareColor;
+
+  rightBar = document.getElementById(`bar-${rightIdx - 1}`);
+  rightBar.style.background = compareColor;
 
   while (i < leftArr.length && j < rightArr.length) {
-    /*bar1 = document.getElementById(`bar-${leftIdx + i}`);
-    bar2 = document.getElementById(`bar-${middle + j}`);
-
-    changeColor(bar1, bar2, compareColor);*/
-    await asyncTimeout({ timeout: sortSpeed / 4 });
-
+    await asyncTimeout({ timeout: sortSpeed / 3 });
     if (leftArr[i] <= rightArr[j]) {
+      if (bar1) bar1.style.backgroundColor = prevColor;
+      if (leftIdx !== leftIdx + i)
+        bar1 = document.getElementById(`bar-${leftIdx + i}`);
+      else bar1 = document.getElementById(`bar-${leftIdx + i + 1}`);
+      bar1.style.backgroundColor = iterColor;
       tempArray.push(leftArr[i]);
       i++;
     } else {
+      if (bar2) bar2.style.backgroundColor = prevColor;
+      bar2 = document.getElementById(`bar-${middle + j}`);
+      bar2.style.backgroundColor = iterColor;
       tempArray.push(rightArr[j]);
       j++;
     }
   }
+  if (bar1) bar1.style.backgroundColor = prevColor;
+  if (bar2) bar2.style.backgroundColor = prevColor;
 
   while (i < leftArr.length) {
-    /*bar1 = document.getElementById(`bar-${leftIdx + i}`);
-     bar2 = document.getElementById(`bar-${middle + j}`);
-     console.log(bar1, bar2, middle, j);
-     changeColor(bar1, bar2, compareColor);*/
-    await asyncTimeout({ timeout: sortSpeed / 4 });
     tempArray.push(leftArr[i]);
     i++;
   }
   while (j < rightArr.length) {
-    /* bar1 = document.getElementById(`bar-${leftIdx + i}`);
-       bar2 = document.getElementById(`bar-${middle + j}`);
-      changeColor(bar1, bar2, compareColor);*/
-    await asyncTimeout({ timeout: sortSpeed / 4 });
     tempArray.push(rightArr[j]);
     j++;
   }
   let newBar;
+
+  let bar3 = document.getElementById(`bar-${middle}`);
+  bar3.style.background = compareColor;
+
   for (let iter = leftIdx, k = 0; iter < rightIdx; iter++, k++) {
-    /*bar1 = document.getElementById(`bar-${leftIdx + i - 1}`);
-    bar2 = document.getElementById(`bar-${middle + j - 1}`);
-    newBar = document.getElementById(`bar-${iter}`);
-    newBar.style.background = prevColor;
-    changeColor(bar1, bar2, compareColor);*/
-    await asyncTimeout({ timeout: sortSpeed / 4 });
     arrayCopy[iter] = tempArray[k];
+    await asyncTimeout({ timeout: sortSpeed / 3 });
+    if (newBar) newBar.style.background = prevColor;
+    if (iter !== leftIdx) {
+      newBar = document.getElementById(`bar-${iter}`);
+      newBar.style.background = iterColor;
+    }
     setRandomArray(arrayCopy.concat());
+    await asyncTimeout({ timeout: sortSpeed / 3 });
   }
+  leftBar.style.background = prevColor;
+  rightBar.style.background = prevColor;
+  newBar.style.background = prevColor;
 };
 
 export default mergeSort;
